a58893f78383f22cbb2f00c00f9a06bca18301b4
mgulho <Marcos_Vinicius_Gulhotti@anfcorp.com>
2025-01-29 14:48:56 -0300

    feat: add Card component with styling and image support

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import './Card.css';
+
+type CardProps = {
+    title: string;
+    description: string;
+    image: string;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export function Card({ title, description, image, ...rest }: CardProps) {
+    return (
+        <div className="card-container" {...rest}>
+            <div className="card-image-container">
+                <Image
+                    src={image}
+                    alt={title}
+                    className="card-image"
+                    width={150}
+                    height={150}
+                />
+            </div>
+            <div className="card-content">
+                <h2 className="card-title">{title}</h2>
+                <p className="card-description">{description}</p>
+            </div>
+        </div>
+    );
+}
